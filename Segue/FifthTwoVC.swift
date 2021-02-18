984ec8586eac5680bbe3f0e10e2aaf6911516ba8
ihValery <ihValery@gmail.com>
2021-02-18 14:38:28 +0300

    Передача с помощью делегатов в обе стороны без segue

--- Segue/FifthTwoVC.swift
+++ Segue/FifthTwoVC.swift
@@ -1,24 +1,22 @@
-//
-//  FifthTwoVC.swift
-//  Segue
-//
-//  Created by Валерий Игнатьев on 17.02.2021.
-//
-
 import UIKit
 
 class FifthTwoVC: UIViewController {
-    
+
     var text = ""
-    weak var delegate: FifthVC?
+    var textForLabel = ""
+    
+    //создаем ту самую слабую ссылку на протокол weak var delegate: FirstViewControllerDelegate
+    weak var delegate: FifthVCDelegate?
     
     @IBOutlet weak var textFieldTwo: UITextField!
     @IBOutlet weak var textLabelTwo: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        textLabelTwo.text = textForLabel
     }
+    
     @IBAction func goBackAction(_ sender: UIButton) {
         takingValueField()
         delegate?.updateLabel(text: text)
@@ -26,37 +24,7 @@ class FifthTwoVC: UIViewController {
     }
     
     func takingValueField() {
-        if let text = textFieldTwo.text, text != "" {
-            self.text = text
-        } else {
-            text = "ты оставил пустое поле"
-        }
+        if let text = textFieldTwo.text, text != "" { self.text = text }
+        else { text = "ты оставил пустое поле" }
     }
 }
-
-
-//import UIKit
-//
-//class FourthTwoVC: UIViewController {
-//
-//    var text = ""
-//    //создаем ту самую слабую ссылку на протокол weak var delegate: FirstViewControllerDelegate
-//    weak var delegate: FourthVC?
-//
-//    @IBOutlet weak var textField: UITextField!
-//    @IBOutlet weak var textLabel: UILabel!
-//
-//    @IBAction func backAction(_ sender: Any) {
-//        takingValueField()
-//        delegate?.update(text: text)
-//        navigationController?.popViewController(animated: true)
-//    }
-//
-//    func takingValueField() {
-//        if let text = textField.text, text != "" {
-//            self.text = text
-//        } else {
-//            text = "ты оставил пустое поле"
-//        }
-//    }
-//}
--- Segue/FifthVC.swift
+++ Segue/FifthVC.swift
@@ -1,10 +1,3 @@
-//
-//  FifthVC.swift
-//  Segue
-//
-//  Created by Валерий Игнатьев on 17.02.2021.
-//
-
 import UIKit
 
 protocol FifthVCDelegate: AnyObject {
@@ -12,50 +5,28 @@ protocol FifthVCDelegate: AnyObject {
 }
 
 class FifthVC: UIViewController, FifthVCDelegate {
-
+    var textOne = ""
+    
     @IBOutlet weak var textFieldOne: UITextField!
     @IBOutlet weak var textLabelOne: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-    }
     @IBAction func goForwardAction(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let fifthTwoVC = storyboard.instantiateViewController(identifier: "FifthTwoSB") as? FifthTwoVC else { return }
-        
+        fifthTwoVC.textForLabel = takingValueFieldOne()
         fifthTwoVC.delegate = self
+        
         show(fifthTwoVC, sender: nil)
     }
     
+    //Выполняем наш метод от подписанного протокола
     func updateLabel(text: String) {
         textLabelOne.text = text
     }
     
+    func takingValueFieldOne() -> String {
+        if let text = textFieldOne.text, text != "" { textOne = text }
+        else { textOne = "с пустыми руками? тогда чай без сахара )))" }
+        return textOne
+    }
 }
-
-
-
-//import UIKit
-//
-////Подписываем его под class (что означает AnyObjects). Это позволит в будущем создать слабую (weak) ссылку на протокол.
-//protocol FourthVCDelegate: AnyObject {
-//
-//    func update(text: String)
-//}
-//
-////Подписываем класс по протокол
-//class FourthVC: UIViewController, FourthVCDelegate {
-//    
-//    @IBOutlet weak var textLabel: UILabel!
-//
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        guard let fourthTwo = segue.destination as? FourthTwoVC else { return }
-//        fourthTwo.delegate = self
-//    }
-//    
-//    //Выполняем наш метод от подписанного протокола
-//    func update(text: String) {
-//        textLabel.text = text
-//    }
-//}
